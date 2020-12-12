e9e34aa0981c294b85ee64706cb02a1f83b59802
Luke Becker <luke.w.becker32@gmail.com>
2020-12-12 10:13:25 -0600

    wrangle update

---
+++ wrangle-dota.py
@@ -0,0 +1,15 @@
+
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+import seaborn as sns
+
+# Importing the os library specifically for reading the csv once I've created the file in my working directory.
+import os
+
+# libraries needed for preparing the data:
+from sklearn.model_selection import train_test_split
+from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import StandardScaler, QuantileTransformer, PowerTransformer, RobustScaler, MinMaxScaler
+
+
